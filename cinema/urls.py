2e35c7281885b0e4a6b0b9dc92377902ac80c419
Alex Bul <tarasov57228@gmail.com>
2025-03-31 21:55:00 +0300

    'Solution'

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,53 +1,98 @@
 from django.urls import path, include
-from rest_framework import routers
+# from rest_framework import routers
+#
+# from cinema.views import (
+#     GenreList,
+#     GenreDetail,
+#     ActorList,
+#     ActorDetail,
+#     MovieViewSet,
+#     CinemaHallViewSet
+# )
+#
+# cinema_hall_list = CinemaHallViewSet.as_view(
+#     actions={
+#         "get": "list",
+#         "post": "create"
+#     }
+# )
+#
+# cinema_hall_detail = CinemaHallViewSet.as_view(
+#     actions={
+#         "get": "retrieve",
+#         "put": "update",
+#         "patch": "partial_update",
+#         "delete": "destroy"
+#     }
+# )
+#
+# router = routers.DefaultRouter()
+#
+# router.register("movies", MovieViewSet, basename="movies")
+#
+#
+# urlpatterns = [
+#     path("genre/", GenreList.as_view(), name="genre_list"),
+#     path("genre/<int:pk>/", GenreDetail.as_view(), name="genre_detail"),
+#
+#     path("actor/", ActorList.as_view(), name="actor_list"),
+#     path("actor/<int:pk>/", ActorDetail.as_view(), name="actor_detail"),
+#
+#     path("cinemahall/", cinema_hall_list, name="cinema_hall_list"),
+#     path(
+#         "cinemahall/<int:pk>/",
+#         cinema_hall_detail,
+#         name="cinema_hall_detail"
+#     ),
+#
+#     path("", include(router.urls)),
+#
+# ]
+#
+# app_name = "cinema"
 
-from cinema.models import Movie
 from cinema.views import (
     GenreList,
     GenreDetail,
     ActorList,
     ActorDetail,
-    MovieViewSet,
-    CinemaHallViewSet
+    CinemaHallViewSet,
+    MovieViewSet
 )
 
-cinema_hall_list = CinemaHallViewSet.as_view(
-    actions={
-        "get": "list",
-        "post": "create"
-    }
-)
+movie_list = MovieViewSet.as_view(actions={
+    "get": "list",
+    "post": "create",
+})
 
-cinema_hall_detail = CinemaHallViewSet.as_view(
-    actions={
-        "get": "retrieve",
-        "put": "update",
-        "patch": "partial_update",
-        "delete": "destroy"
-    }
-)
-
-router = routers.DefaultRouter()
-
-router.register("movies", MovieViewSet, basename="movies")
-
+movie_detail = MovieViewSet.as_view(actions={
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy"
+})
 
 urlpatterns = [
-    path("genre/", GenreList.as_view(), name="genre_list"),
-    path("genre/<int:pk>/", GenreDetail.as_view(), name="genre_detail"),
-
-    path("actor/", ActorList.as_view(), name="actore_list"),
-    path("actor/<int:pk>/", ActorDetail.as_view(), name="actor_detail"),
-
-    path("cinemahall/", cinema_hall_list, name="cinema_hall_list"),
-    path(
-        "cinemahall/<int:pk>/",
-        cinema_hall_detail,
-        name="cinema_hall_detail"
-    ),
-
-    path("", include(router.urls)),
-
+    path("movies/", movie_list, name="movie-list"),
+    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_halls/", CinemaHallViewSet.as_view(
+        actions={
+            "get": "list",
+            "post": "create",
+        }
+    ), name="cinema-hall-list"),
+    path("cinema_halls/<int:pk>/", CinemaHallViewSet.as_view(
+        actions={
+            "get": "retrieve",
+            "put": "update",
+            "patch": "partial_update",
+            "delete": "destroy"
+        }
+    ), name="cinema-hall-detail"),
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -15,49 +15,51 @@ from cinema.serializers import (
 
 
 class GenreList(APIView):
-
-    def get(self, request):
+    def get(self, request) -> Response:
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
+    def post(self, request) -> Response:
         serializer = GenreSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 class GenreDetail(APIView):
+    def get_object(self, pk: int) -> Genre:
+        return get_object_or_404(Genre, pk=pk)
 
-    def get_object(self, pk):
-        genres = get_object_or_404(Genre, pk=pk)
-        return genres
-
-    def get(self, request, pk):
-        genre = self.get_object(pk)
+    def get(self, request, pk: int) -> Response:
+        genre = self.get_object(pk=pk)
         serializer = GenreSerializer(genre)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def put(self, request, pk):
-        genre = self.get_object(pk)
+    def put(self, request, pk: int) -> Response:
+        genre = self.get_object(pk=pk)
         serializer = GenreSerializer(genre, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
-    def delete(self, request, pk):
-        genre = self.get_object(pk)
-        genre.delete()
+    def patch(self, request, pk: int) -> Response:
+        genre = self.get_object(pk=pk)
+        serializer = GenreSerializer(genre, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, pk: int) -> Response:
+        self.get_object(pk=pk).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ActorList(
-    generics.GenericAPIView,
     mixins.ListModelMixin,
-    mixins.CreateModelMixin
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
 ):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
@@ -70,9 +72,9 @@ class ActorList(
 
 
 class ActorDetail(
-    mixins.DestroyModelMixin,
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
     generics.GenericAPIView
 ):
     queryset = Actor.objects.all()
@@ -84,8 +86,12 @@ class ActorDetail(
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
 
+    def patch(self, request, *args, **kwargs) -> Response:
+        return self.partial_update(request, *args, **kwargs)
+
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
 
 
 class CinemaHallViewSet(
